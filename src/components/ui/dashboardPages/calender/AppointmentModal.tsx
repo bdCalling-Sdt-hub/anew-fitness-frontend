5683e5c600aaf1091312806ec4395d096c91485f
Mithila Khan <mithilakhan082@gmail.com>
2025-04-10 16:45:48 +0600

    fsdgs

--- src/components/ui/dashboardPages/calender/AppointmentModal.tsx
+++ src/components/ui/dashboardPages/calender/AppointmentModal.tsx
@@ -1,17 +1,98 @@
 import { Button, DatePicker, Form, Modal, Select } from "antd";
 import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { useAddAppointmentContactMutation, useGetAllAppointmentContactQuery, useUpdateAppointmentContactMutation } from "../../../../redux/features/contact/appointmentClientApi";
 
-const AppointmentModal = ({open , setOpen , setModalOpen }:{open: boolean, setModalOpen?: (modalOpen: boolean) => void , setOpen: (open: boolean) => void}) => { 
+const AppointmentModal = ({open , setOpen , setModalOpen , setEditAppointmentData , editAppointmentData }:{open: boolean, setModalOpen?: (modalOpen: boolean) => void , setOpen: (open: boolean) => void , editAppointmentData?: any , setEditAppointmentData:(editAppointmentData: any)=>void}) => { 
+
+        const [form] = Form.useForm();
+    
+        const [addAppointmentContact, { isError, isLoading, isSuccess, data, error }] = useAddAppointmentContactMutation();
+    
+        const [updateAppointmentContact, { isError: isUpdateError, isLoading: isUpdateLoading, isSuccess: isUpdateSuccess, data: updateData, error: updateError }] = useUpdateAppointmentContactMutation(); 
+    
+        const {data:allAppointment , refetch} = useGetAllAppointmentContactQuery(undefined);
+    
+                  useEffect(() => {
+                      if (isSuccess) {
+                          if (data) {
+                              Swal.fire({
+                                  text: data?.message,
+                                  icon: "success",
+                                  timer: 1500,
+                                  showConfirmButton: false
+                              }).then(() => {
+                                  setOpen(false); 
+                                  refetch();  
+                                  form.resetFields(); 
+                              })
+                          }
+                      }
+                      if (isError) {
+                          Swal.fire({
+                              //@ts-ignore
+                              text: error?.data?.message,
+                              icon: "error",
+                          });
+                      }
+                  }, [isSuccess, isError, error, data]);    
+            
+            
+                  useEffect(() => {
+                      if (isUpdateSuccess) {
+                          if (updateData) {
+                              Swal.fire({
+                                  text: updateData?.message,
+                                  icon: "success",
+                                  timer: 1500,
+                                  showConfirmButton: false
+                              }).then(() => {
+                                  setOpen(false);  
+                                  setEditAppointmentData({})
+                                  refetch();  
+                                  form.resetFields(); 
+                              })
+                          }
+                      }
+                      if (isUpdateError) {
+                          Swal.fire({
+                              //@ts-ignore
+                              text: updateError?.updateData?.message,
+                              icon: "error",
+                          });
+                      }
+                  }, [isUpdateSuccess, isUpdateError, updateError, updateData]);     
+    
+        useEffect(() => {
+            if (editAppointmentData) {
+                form.setFieldsValue(editAppointmentData);
+            }
+        }, [editAppointmentData])
+     
 
     useEffect(() => {
         if (open && setModalOpen) {
           setModalOpen(false);
         }
-      }, [open, setModalOpen]); 
+      }, [open, setModalOpen]);  
+
+
+
+      const OnFinish = (values:{ client_name:string , client_email:string , gender:string , address:string , mobile_number:string}) => {
+ 
+        console.log(editAppointmentData?.id);
+        if (editAppointmentData?.id) {
+            updateAppointmentContact({ id: editAppointmentData?.id, data: values }).then((res) => { console.log(res);});
+        } else {
+
+            addAppointmentContact(values).then((res) => { console.log(res);});
+        }
+    } 
+
 
     return (
-        <Modal open={open}  onCancel={() => setOpen(false)} footer={null} width={600} title={<p className=" text-primary text-[24px] font-bold">Book Appointment </p>} centered>
-            <Form layout="vertical"> 
+        <Modal open={open}  onCancel={() =>{ setOpen(false); setEditAppointmentData({})}} footer={null} width={600} title={<p className=" text-primary text-[24px] font-bold">Book Appointment </p>} centered>
+            <Form layout="vertical" form={form} onFinish={OnFinish}> 
                 <div className=""> 
                 <Form.Item name="contact" label={<p className=" text-primaryText text-[18px] font-semibold"> Contact </p>}>
                     <Select
@@ -93,7 +174,7 @@ const AppointmentModal = ({open , setOpen , setModalOpen }:{open: boolean, setMo
 
                 <div className="flex  items-center justify-end gap-4 mt-4">
                     <Button onClick={() => setOpen(false)}>Close</Button>
-                    <button className='px-5 py-[6px] text-white bg-primary rounded' type="submit">Save</button>
+                    <Button htmlType="submit" className='px-5 py-[6px] text-white bg-primary rounded' >{isUpdateLoading || isLoading ? "Saving.." : "Save"}</Button>
                 </div>
 
             </Form>
