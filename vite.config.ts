b0e553f4759c7b4d7b505197bf97fc83516b9c8d
Mithila Khan <mithilakhan082@gmail.com>
2025-04-29 12:30:34 +0600

    sdfs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],  
+  resolve: {
+    alias: {
+      '@fullcalendar/resource': '@fullcalendar/resource-timegrid', // force it to resolve correctly
+    },
+  },
   server: {
 
     host: '72.167.224.54',     
