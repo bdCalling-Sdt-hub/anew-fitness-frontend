2194ecb8d5a758fa6b6ee3c96cffa71740bd29be
Mithila Khan <mithilakhan082@gmail.com>
2025-04-29 17:15:22 +0600

    build-issues-solve

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],  
-  resolve: {
-    alias: {
-      '@fullcalendar/resource': '@fullcalendar/resource-timegrid', // force it to resolve correctly
-    },
-  },
   server: {
 
-    host: '10.0.70.92',     
+    host: '72.167.224.54',     
     port: 8080,      
   },
 })
